b9caed876741ec888b928eb7ce2bcb1694498bbb
Alex March <alexmarch@fastmail.com>
2023-10-20 14:22:38 +0900

    fix(cmp): add formatting to cmp sources

--- lua/hosaka/config/luasnip.lua
+++ lua/hosaka/config/luasnip.lua
@@ -1,5 +1,8 @@
 local luasnip = require("luasnip")
-luasnip.config.set_config({ history = true })
+luasnip.config.set_config({
+  history = true,
+  delete_check_events = "TextChanged",
+})
 
 -- load local snippets
 require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/misc/snippets" } })
--- lua/hosaka/config/nvim-cmp.lua
+++ lua/hosaka/config/nvim-cmp.lua
@@ -1,5 +1,7 @@
 local cmp = require("cmp")
 
+vim.api.nvim_set_hl(0, "GhostText", { link = "Comment", default = true })
+
 cmp.setup({
   sources = {
     { name = "nvim_lsp" },
@@ -16,6 +18,30 @@ cmp.setup({
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
     end,
+  },
+  -- pre-select first item
+  -- completion = {
+  --   completeopt = "menu,menuone,noinsert",
+  -- },
+  formatting = {
+    fields = { "abbr", "menu", "kind" },
+    format = function(entry, item)
+      local short_name = {
+        nvim_lsp = "lsp",
+        nvim_lua = "nvim",
+        luasnip = "snip",
+      }
+
+      local menu_name = short_name[entry.source.name] or entry.source.name
+      item.menu = string.format("[%s]", menu_name)
+      return item
+    end,
+  },
+  experimental = {
+    -- NOTE: set this to false if using copilot.lua
+    ghost_text = {
+      hl_group = "GhostText",
+    },
   },
 })
 
