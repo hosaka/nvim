80b1055564a4278554d1072fe276a9726253df71
Alex March <alexmarch@fastmail.com>
2023-12-14 14:06:54 +0900

    fix: add <leader>q to diffview to simplify tabclose

--- lua/hosaka/config/diffview.lua
+++ lua/hosaka/config/diffview.lua
@@ -1,1 +1,12 @@
-require("diffview").setup({})
+local tabclose = { "n", "<leader>q", [[<cmd>tabclose<cr>]], { desc = "Close Diffview" } }
+
+require("diffview").setup({
+  keymaps = {
+    view = {
+      tabclose
+    },
+    file_panel = {
+      tabclose
+    }
+  }
+})
--- lua/hosaka/config/neogit.lua
+++ lua/hosaka/config/neogit.lua
@@ -1,9 +1,9 @@
 require("neogit").setup({
-  kind = "split",
+  kind = "tab",
   graph_style = "unicode",
   disable_hint = true,
   disable_commit_confirmation = true,
   integrations = {
     diffview = true
-  }
+  },
 })
