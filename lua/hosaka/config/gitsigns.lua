453e9ced599f28dd93d2bdfd4dc9e88ea06173d4
Alex March <alexmarch@fastmail.com>
2023-12-27 12:29:39 +0900

    feat: add binding to show deleted git hunks

--- lua/hosaka/config/gitsigns.lua
+++ lua/hosaka/config/gitsigns.lua
@@ -1,5 +1,12 @@
 local gs = require("gitsigns")
 gs.setup({
+  signs = {
+    add = { text = "+" },
+    change = { text = "~" },
+    delete = { text = "_" },
+    topdelete = { text = "?" },
+    changedelete = { text = "~" },
+  },
   on_attach = function(buffer)
     local map = function(mode, lhs, rhs, desc)
       local opts = {}
@@ -13,7 +20,9 @@ gs.setup({
     end
 
     vim.keymap.set("n", "]h", function()
-      if vim.wo.diff then return "]h" end
+      if vim.wo.diff then
+        return "]h"
+      end
       vim.schedule(function()
         gs.next_hunk()
       end)
@@ -21,7 +30,9 @@ gs.setup({
     end, { expr = true, desc = "Hunk forward" })
 
     vim.keymap.set("n", "[h", function()
-      if vim.wo.diff then return "[h" end
+      if vim.wo.diff then
+        return "[h"
+      end
       vim.schedule(function()
         gs.prev_hunk()
       end)
@@ -32,16 +43,25 @@ gs.setup({
     vim.keymap.set({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<cr>")
 
     mapl("n", "ga", [[<cmd>lua require("gitsigns").stage_hunk()<cr>]], "Add hunk")
-    mapl("v", "ga", [[<cmd>lua require("gitsigns").stage_hunk({vim.fn.line("."), vim.fn.line("v")})<cr>]],
-      "Add hunk selection")
+    mapl(
+      "v",
+      "ga",
+      [[<cmd>lua require("gitsigns").stage_hunk({vim.fn.line("."), vim.fn.line("v")})<cr>]],
+      "Add hunk selection"
+    )
     mapl("n", "gA", [[<cmd>lua require("gitsigns").stage_buffer()<cr>]], "Add buffer")
     mapl("n", "gb", [[<cmd>lua require("gitsigns").blame_line()<cr>]], "Blame line")
     mapl("n", "gp", [[<cmd>lua require("gitsigns").preview_hunk_inline()<cr>]], "Preview hunk")
     mapl("n", "gr", [[<cmd>lua require("gitsigns").reset_hunk()<cr>]], "Reset hunk")
-    mapl("v", "gr", [[<cmd>lua require("gitsigns").reset_hunk({vim.fn.line("."), vim.fn.line("v")})<cr>]],
-      "Reset hunk selection")
+    mapl(
+      "v",
+      "gr",
+      [[<cmd>lua require("gitsigns").reset_hunk({vim.fn.line("."), vim.fn.line("v")})<cr>]],
+      "Reset hunk selection"
+    )
     mapl("n", "gR", [[<cmd>lua require("gitsigns").reset_buffer()<cr>]], "Reset buffer")
     mapl("n", "gu", [[<cmd>lua require("gitsigns").undo_stage_hunk()<cr>]], "Undo add hunk")
     mapl("n", "gq", [[<cmd>lua require("gitsigns").setqflist()<cr>:open<cr>]], "Quickfix hunks")
+    mapl("n", "gs", [[<cmd>lua require("gitsigns").toggle_deleted()<cr>]], "Show deleted hunks")
   end,
 })
