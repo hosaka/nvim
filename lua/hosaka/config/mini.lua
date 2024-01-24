8523b59797b5a989c44257f399908fb623ecd45b
Alex March <alexmarch@fastmail.com>
2024-01-24 16:01:32 +0900

    fix: filter out noisy lua_ls notifications

--- lua/hosaka/config/mini.lua
+++ lua/hosaka/config/mini.lua
@@ -3,11 +3,21 @@ local now, later = hosaka.now, hosaka.later
 -- immediate config
 now(function()
   local notify = require("mini.notify")
+
+  local notify_filter = function(notif_arr)
+    local lua_ls = function(notif)
+      return not (vim.startswith(notif.msg, "lua_ls: Diagnosing") or vim.startswith(notif.msg, "lua_ls: Processing"))
+    end
+    notif_arr = vim.tbl_filter(lua_ls, notif_arr)
+    return notify.default_sort(notif_arr)
+  end
+
   notify.setup({
     content = {
       format = function(notif)
         return notif.msg
       end,
+      sort = notify_filter,
     },
     window = {
       config = {
