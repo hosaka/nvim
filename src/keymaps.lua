53992c7753d011761cd1cd8d7cbb0a7162615795
Alex March <alexmarch@fastmail.com>
2024-02-20 12:00:15 +0900

    fix: add desc to basic keymaps where they were missing

--- src/keymaps.lua
+++ src/keymaps.lua
@@ -13,7 +13,7 @@ map({ "n", "x" }, "[p", [[<cmd>exe 'put! ' . v:register<cr>]], { desc = "Paste a
 map({ "n", "x" }, "]p", [[<cmd>exe 'put ' . v:register<cr>]], { desc = "Paste below" })
 
 -- cancel search highlight
-map({ "i", "n" }, [[<Esc>]], [[<cmd>nohlsearch<cr><esc>]])
+map({ "i", "n" }, [[<Esc>]], [[<cmd>nohlsearch<cr><esc>]], { desc = "Cancel search highlight" })
 
 -- delete selection with backspace in select mode
-map("s", [[<BS>]], [[<BS>i]])
+map("s", [[<BS>]], [[<BS>i]], { desc = "Delete selection" })
