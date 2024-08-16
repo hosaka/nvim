47d0f6bc2a364d1479a0340b6907cbc9461dad3e
Alex March <alexmarch@fastmail.com>
2024-08-16 23:05:10 +0900

    chore: remove unused plugin configs

--- src/plugins/aerial.lua
+++
@@ -1,9 +0,0 @@
-require("aerial").setup({
-  attach_mode = "global",
-  backends = { "lsp", "treesitter", "markdown", "man" },
-  layout = {
-    min_width = 42,
-  },
-  show_guides = true,
-  filter_kind = false,
-})
