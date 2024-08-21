c8dea314f0cfd91daebd2291c0260c6cb538f17a
Alex March <alexmarch@fastmail.com>
2024-08-21 12:43:52 +0900

    fix: no not preview large files in quickfix list

--- src/plugins/nvim-bqf.lua
+++ src/plugins/nvim-bqf.lua
@@ -1,4 +1,16 @@
 require("bqf").setup({
+  preview = {
+    border = "rounded",
+    should_preview_cb = function(buffer, _)
+      local bufname = vim.api.nvim_buf_get_name(buffer)
+      local fsize = vim.fn.getfsize(bufname)
+      -- skip files over 100k
+      if fsize > 100 * 1024 then
+        return false
+      end
+      return true
+    end,
+  },
   func_map = {
     split = "<C-s>",
     vsplit = "<C-v>",
