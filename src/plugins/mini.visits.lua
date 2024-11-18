09551d712f23b098521dc64aafc0e803e84d077e
Alex March <alexmarch@fastmail.com>
2024-11-18 13:38:47 +0900

    fix: more details mini.visits pickers

--- src/plugins/mini.visits.lua
+++ src/plugins/mini.visits.lua
@@ -3,30 +3,31 @@ minivisits.setup()
 
 local minipick = require("mini.pick")
 
-local function pick_visits(label, cwd)
-  return function()
-    local miniextra = require("mini.extra")
-    miniextra.pickers.visit_paths({
-      cwd = cwd,
-      filter = label,
-      recency_weight = 0,
-    }, {
-      source = { name = string.format("%s visits (%s)", label, cwd ~= "" and "cwd" or "all") },
-      mappings = {
-        delete_visit = {
-          char = "<C-d>",
-          func = function()
-            local matches = minipick.get_picker_matches()
-            if matches and matches.current then
-              minivisits.remove_label(label, matches.current, cwd)
+local function pick_visit_labels(opts)
+  local picker_opts = {
+    mappings = {
+      delete_label = {
+        char = "<C-d>",
+        func = function()
+          local matches = minipick.get_picker_matches()
+          if matches then
+            if next(matches.marked) then
+              for _, label in ipairs(matches.marked) do
+                minivisits.remove_label(label)
+              end
+            elseif matches.current then
+              minivisits.remove_label(matches.current)
             end
-            pick_visits(label, cwd)()
-          end,
-        },
+            pick_visit_labels(opts)
+          end
+        end,
       },
-    })
-  end
+    },
+  }
+  return require("mini.extra").pickers.visit_labels(opts, picker_opts)
 end
+
+minipick.registry.visit_labels = pick_visit_labels
 
 local iterate_visits = function(label, direction)
   return function()
@@ -34,10 +35,38 @@ local iterate_visits = function(label, direction)
   end
 end
 
+local function pick_visits(opts)
+  local cwd = opts.cwd
+  local label = opts.label
+  local picker_opts = {
+    source = {
+      name = string.format("%s visits (%s)", label, cwd ~= "" and "cwd" or "all"),
+    },
+    mappings = {
+      delete_visit = {
+        char = "<C-d>",
+        func = function()
+          local matches = minipick.get_picker_matches()
+          if matches then
+            if next(matches.marked) then
+              for _, marked in ipairs(matches.marked) do
+                minivisits.remove_label(label, marked, cwd)
+              end
+            elseif matches.current then
+              minivisits.remove_label(label, matches.current, cwd)
+            end
+            pick_visits(opts)
+          end
+        end,
+      },
+    },
+  }
+  return require("mini.extra").pickers.visit_paths({ cwd = cwd, filter = label, recency_weight = 0 }, picker_opts)
+end
+
+minipick.registry.visits = pick_visits
+
 local map = Hosaka.keymap.map
-local mapl = Hosaka.keymap.mapl
 
 map("]]", iterate_visits("core", "forward"), { desc = "Core forward" })
 map("[[", iterate_visits("core", "backward"), { desc = "Core backward" })
-mapl("vc", pick_visits("core", ""), { desc = "Core visits (all)" })
-mapl("vC", pick_visits("core", nil), { desc = "Core visits (cwd)" })
