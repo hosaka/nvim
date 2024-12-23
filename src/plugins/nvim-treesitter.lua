ccb9e3b917020ab1b9bf3efb5286a3f3b9396615
Alex March <alexmarch@fastmail.com>
2024-12-23 12:40:23 +0900

    feat(treesitter): syntax for justfiles

--- src/plugins/nvim-treesitter.lua
+++ src/plugins/nvim-treesitter.lua
@@ -89,3 +89,13 @@ require("nvim-treesitter.configs").setup({
     },
   },
 })
+
+-- justfile
+require("nvim-treesitter.parsers").get_parser_configs().just = {
+  install_info = {
+    url = "https://github.com/IndianBoy42/tree-sitter-just",
+    files = { "src/parser.c", "src/scanner.c" },
+    branch = "main",
+  },
+  maintainers = { "@IndianBoy42" },
+}
