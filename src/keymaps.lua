1051f7ecb7547c43739b733f82edf6a5d3b181fb
Alex March <alexmarch@fastmail.com>
2024-12-23 12:41:12 +0900

    fix: separate keymap for toggling hlsearch

--- src/keymaps.lua
+++ src/keymaps.lua
@@ -1,12 +1,13 @@
 -- note: a lot of mappings are defined by mini.basics, see `init.lua`
--- note: the rest off the <Leader> mappings are defined in `leadermaps.lua`
+-- note: the rest of the <Leader> mappings are defined in `leadermaps.lua`
 
 -- paste above/below linewise
 vim.keymap.set({ "n", "x" }, "[p", [[<cmd>exe 'put! ' . v:register<cr>]], { desc = "Paste above", silent = true })
 vim.keymap.set({ "n", "x" }, "]p", [[<cmd>exe 'put ' . v:register<cr>]], { desc = "Paste below", silent = true })
 
--- cancel search highlight
-vim.keymap.set({ "i", "n" }, [[<Esc>]], [[<cmd>nohlsearch<cr><esc>]], { desc = "Cancel search", silent = true })
+-- search highlight
+vim.keymap.set("n", [[\h]], [[:let v:hlsearch = 1 - v:hlsearch<cr>]], { desc = "Toggle hlsearch", silent = true })
+vim.keymap.set({ "i", "n" }, [[<Esc>]], [[<cmd>nohlsearch<cr><esc>]], { desc = "Cancel hlsearch", silent = true })
 
 -- delete empty lines to blackhole
 vim.keymap.set("n", "dd", function()
