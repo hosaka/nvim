4ac1f8d902417758ea9a1145e00e681d8c2f942e
Alex March <alexmarch@fastmail.com>
2025-05-13 16:55:22 +0900

    feat: enable mini.splitjoin

--- plugin/20_mini.lua
+++ plugin/20_mini.lua
@@ -28,6 +28,20 @@ now(function()
 end)
 
 now(function()
+  local miniicons = require("mini.icons")
+  miniicons.setup({
+    -- ignore some extensions and rely on `vim.filetype.match()` fallback
+    use_file_extension = function(ext, _)
+      local suf3, suf4 = ext:sub(-3), ext:sub(-4)
+      return suf3 ~= "scm" and suf3 ~= "txt" and suf3 ~= "yml" and suf4 ~= "json" and suf4 ~= "yaml"
+    end,
+  })
+  later(miniicons.mock_nvim_web_devicons)
+  -- note: enable if using mini.completion
+  -- later(miniicons.tweak_lsp_kind)
+end)
+
+now(function()
   local notify = require("mini.notify")
   local notify_filter = function(notif_arr)
     local lua_ls = function(notif)
@@ -92,20 +106,6 @@ now(function()
   require("mini.tabline").setup({
     tabpage_section = "right",
   })
-end)
-
-now(function()
-  local miniicons = require("mini.icons")
-  miniicons.setup({
-    -- ignore some extensions and rely on `vim.filetype.match()` fallback
-    use_file_extension = function(ext, _)
-      local suf3, suf4 = ext:sub(-3), ext:sub(-4)
-      return suf3 ~= "scm" and suf3 ~= "txt" and suf3 ~= "yml" and suf4 ~= "json" and suf4 ~= "yaml"
-    end,
-  })
-  later(miniicons.mock_nvim_web_devicons)
-  -- note: enable if using mini.completion
-  -- later(miniicons.tweak_lsp_kind)
 end)
 
 -- delayed config
@@ -335,6 +335,10 @@ end)
 
 later(function()
   source("config/mini.pick.lua")
+end)
+
+later(function()
+  require("mini.splitjoin").setup()
 end)
 
 later(function()
