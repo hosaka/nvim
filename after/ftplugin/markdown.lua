63a72d62b41529f30a518d1a1eaba88b81a05e26
Alex March <alexmarch@fastmail.com>
2025-10-21 13:54:16 +0900

    feat: custom surround for markdown files

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -14,3 +14,19 @@ end
 -- disable "show table of contents" on nvim>=0.11 in favour
 -- of `gO` from 'mini.basics'
 pcall(vim.keymap.del, "n", "gO", { buffer = 0 })
+
+vim.b.minisurround_config = {
+  custom_surroundings = {
+    -- markdown link, usage:
+    -- `saiwL` - add link
+    -- `sdL` - delete link
+    -- `srLL` - replace link
+    L = {
+      input = { "%[().-()%]%(.-%)" },
+      output = function()
+        local link = require("mini.surround").user_input("Link: ")
+        return { left = "[", right = "](" .. link .. ")" }
+      end,
+    },
+  },
+}
