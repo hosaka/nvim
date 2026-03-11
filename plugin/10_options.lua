e81aac8e9d1c6e2ce415a458afcf6376d2f53584
Alex March <alexmarch@fastmail.com>
2026-03-11 16:46:56 +0900

    fix: remove the has("wsl") check for setting up WslClipboard

--- plugin/10_options.lua
+++ plugin/10_options.lua
@@ -200,22 +200,6 @@ require("mini.deps").later(function()
   })
 end)
 
--- Clipboard
-if vim.fn.has("wsl") then
-  vim.g.clipboard = {
-    name = "WslClipboard",
-    copy = {
-      ["+"] = "clip.exe",
-      ["*"] = "clip.exe",
-    },
-    paste = {
-      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
-    },
-    cache_enabled = 0,
-  }
-end
-
 -- Netrw
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
