141d7ee05db2fc3daf3b52b45f584e5159b7f073
Alex March <alexmarch@fastmail.com>
2026-03-25 18:27:06 +0900

    fix(mini): place notifications in the top right corner
    - They would frequently overlap with the mini.clue

--- plugin/20_mini.lua
+++ plugin/20_mini.lua
@@ -49,20 +49,12 @@ now(function()
     end
     return notify.default_sort(vim.tbl_filter(lua_ls, notif_arr))
   end
-  local window_config = function()
-    local has_statusline = vim.o.laststatus > 0
-    local padding = vim.o.cmdheight + (has_statusline and 1 or 0)
-    return { anchor = "SE", col = vim.o.columns, row = vim.o.lines - padding }
-  end
   notify.setup({
     content = {
       format = function(notif)
         return notif.msg
       end,
       sort = notify_filter,
-    },
-    window = {
-      config = window_config,
     },
   })
 end)
